bb980f19e8c4732dd636d6f43aa00be8180552d4
ToyinH <byhisgraceabba@gmail.com>
2022-10-14 05:37:11 +0000

    code with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,8 @@
 
 /**
  * main - print the phrase "with proper grammar,
- * but the outcome is a piece of art,
- * return: 0 if exited properly, non-zero otherwise
+ * but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
