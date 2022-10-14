916398e1ef1b34fa3deb31f56e9577698b015160
ToyinH <byhisgraceabba@gmail.com>
2022-10-14 06:25:43 +0000

    return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - print exactily 'and that piece of art is useful" -
@@ -9,7 +11,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful \" - Dora Korpar,
-	2015-10-19\n", 59);
+	int s = strien ("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
 	return (1);
-
+}
