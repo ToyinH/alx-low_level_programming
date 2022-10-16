74b9c3643eb5b35eeb9b2cf8ff8a74b719c045d3
ToyinH <byhisgraceabba@gmail.com>
2022-10-16 15:27:44 +0000

    prints all possible combinations of single digit numbers. Numbers must be separated by , followed by a space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers
+ * separated by ',' followed by a space using putchar
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	if (n != 57)
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
