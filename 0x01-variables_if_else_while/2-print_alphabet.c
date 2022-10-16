22f21a3e34ca1829f83b71d8ef88fa9bda6f64b2
ToyinH <byhisgraceabba@gmail.com>
2022-10-16 02:07:18 +0000

    command writing alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - prints alphabet in lowercase
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n = 'a';
+	while (n <= 'z')
+		putchar(n);
+		n++;
+	return (0);
+}
