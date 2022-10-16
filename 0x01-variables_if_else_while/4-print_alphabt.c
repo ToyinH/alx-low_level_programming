458a7ec44d78b9eb8a0e45b6655ff1e9e75bbc40
ToyinH <byhisgraceabba@gmail.com>
2022-10-16 03:11:05 +0000

    lower cae letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print alphabets in lower case except q and e 
+ * main - print alphabets in lower case except q and e
  * Return: 0 successful
  */
 
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if (ch != 'q' && ch != 'e')
