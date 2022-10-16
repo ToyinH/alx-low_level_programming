b3e6d8f2ade33fd62f782e074a615c8e47bea30e
ToyinH <byhisgraceabba@gmail.com>
2022-10-16 01:03:32 +0000

    use of if and boolen

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print single digit of base 10
+ * Return: zero if successful
+ */
+
+int main(void)
+{
+	int n = 0;
+	while (n < 10);
+		printf("%d\n", n);
+		n++;
+	return (0)
+}
