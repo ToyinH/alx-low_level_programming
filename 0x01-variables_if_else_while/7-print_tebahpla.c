5913f535910e518a7526dcd9f94157f80d65c083
ToyinH <byhisgraceabba@gmail.com>
2022-10-16 03:38:08 +0000

    alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case alphabets in reverse order
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int char = ch;
+
+	for (ch = 'z'; ch >= a; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
