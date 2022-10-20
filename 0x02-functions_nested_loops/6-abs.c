f27041ad9d54feafff2a3e0bd7e98584c01910eb
ToyinH <byhisgraceabba@gmail.com>
2022-10-20 04:24:09 +0000

    print absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * _abs - computes absolute value of integer
+ * Return: absolute value 
+ *
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+		return (n);
+	else if (n == 0)
+		return (0);
+	else
+		return (n);
+}
