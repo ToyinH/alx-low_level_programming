ea7acbe36ab2badf3f64e6a41ac9b083a38ec60b
ToyinH <byhisgraceabba@gmail.com>
2022-10-21 14:01:32 +0000

    print diagonal line using putchar

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - draw a diagonal line
+ *@n: the number of times
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		int a;
+
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
