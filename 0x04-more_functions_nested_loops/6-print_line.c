f906610a5fc542fa0da3d5041ee0681277782bc6
ToyinH <byhisgraceabba@gmail.com>
2022-10-21 12:24:29 +0000

    draw a line using putchar

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draw a straight line in the terminal
+ * @n: number of times
+ *
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('-');
+	}
+}
+
