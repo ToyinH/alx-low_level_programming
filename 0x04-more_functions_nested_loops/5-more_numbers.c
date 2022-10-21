c94efa642634b79e4216f8711dae6a68a50742f7
ToyinH <byhisgraceabba@gmail.com>
2022-10-21 04:55:33 +0000

    numbers printed 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times number
+ *
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
