ccab7cb9242426391565c993f871a59c4ac923d7
ToyinH <byhisgraceabba@gmail.com>
2022-10-21 06:22:42 +0000

    putchar 1 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,13 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 48; b <= 62; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b >= 10)
+			{
+				_putchar(b%10 + '0');
+			}
+			_putchar(b/10 + '0');
 		}
 		_putchar('\n');
 	}
