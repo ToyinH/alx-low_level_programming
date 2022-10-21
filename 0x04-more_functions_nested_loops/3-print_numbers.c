d6e5deba0d5de68b306598e2581d8befde8b7b99
ToyinH <byhisgraceabba@gmail.com>
2022-10-21 03:51:46 +0000

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9 followed by a new line
+ * Return: 0 if succesful
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar('c');
+}
