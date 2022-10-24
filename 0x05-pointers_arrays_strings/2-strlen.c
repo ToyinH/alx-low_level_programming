2ec5d711c5d9f976409d2b7b40b590ba9f64a0d0
ToyinH <byhisgraceabba@gmail.com>
2022-10-24 21:12:44 +0000

    calculate string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,14 @@
 /**
  * _strlen - function that returns the length of a string
  * @s: character
- * @i: length of string
  * Return: return the string length
  *
  */
 
 int _strlen(char *s)
 {
-	int i;
-
 	i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
