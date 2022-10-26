cfac318d13d03e2e310fbff180208e7a81180e3f
ToyinH <byhisgraceabba@gmail.com>
2022-10-26 13:27:50 +0000

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,7 @@
 void rev_string(char *s)
 {
 	int i = 0, j;
+	char temp;
 
 	while (s[i] != '\0')
 	{
@@ -13,6 +14,8 @@ void rev_string(char *s)
 	}
 	for (j = i - 1; j >= 0; j--)
 	{
-		s[j];
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
 }
