774534072bce5a75767ee6e17af691b8ffb2de38
ToyinH <byhisgraceabba@gmail.com>
2022-10-26 14:31:03 +0000

    print second half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,15 @@ void puts_half(char *str)
 {
 	int i = 0, j;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		i++
+		i++;
 	}
 	j -= i / 2;
 	
 	while (j != '\0')
 	{
-		_putchar(s[j]);
+		_putchar(str[j]);
 		j++;
 	}
 	_putchar('\n');
