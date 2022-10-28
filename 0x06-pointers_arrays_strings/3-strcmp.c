fc43b68475c419f5623bb1c8943de4219afd0127
ToyinH <byhisgraceabba@gmail.com>
2022-10-28 03:11:14 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,25 @@
  * _strcmp - function that compares two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if same
+ * Return: 0 if same, 1 if s1 is greater and -1 if s1 is less
  *
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[i] && s1[i] > s2[i])
 		{
 			return (1);
 		}
-		else
+		else if (s1[i] != s2[i] && s1[i] < s2[i])
 		{
-			return (0);
+			return (-1);
 		}
+		else
+			return (0);
 	}
 }
