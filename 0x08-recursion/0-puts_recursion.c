8579b15219a7d1c7ee553b9c8a5cc6d9fafc48dc
ToyinH <byhisgraceabba@gmail.com>
2022-11-02 13:58:58 +0000

    func

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  * @s: string
  *
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
+		_putchar(*s + *(s + 1));
 	}
-	_putchar(*s + *(s + 1));
+	_putchar('\n');
 }
