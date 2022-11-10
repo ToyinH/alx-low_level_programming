83653f7cf20a0c9485aee903a8111a42a755d7ce
ToyinH <byhisgraceabba@gmail.com>
2022-11-10 03:58:05 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,15 +18,11 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 	for (i = 0; str[i] != '\0'; i++)
-	{
-		a = malloc(i * sizeof(*a) + 1);
-		if (a == NULL)
-			return (NULL);
-	}
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
 	for (b = 0; b < i; b++)
-	{
 		a[b] = str[b];
-	}
 	a[b] = '\0';
 	return (a);
 }
