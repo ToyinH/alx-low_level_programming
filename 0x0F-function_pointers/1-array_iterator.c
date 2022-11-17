595da21c764ba85fe9d724b561c90edbb1c5b874
ToyinH <byhisgraceabba@gmail.com>
2022-11-17 04:39:08 +0000

    function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
 /**
- * int_index - function that searches for an integer
- * @array: array of integers
+ * array_iterator: execute a function pointer on each
+ * element of an array
+ * @array: array of intergers
  * @size: size of array
- * @cmp: function pointer
- * Return: index of first element that matches with 'cmp', or -1 if none
- * found
- *
+ * @action: function pointer
  *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
