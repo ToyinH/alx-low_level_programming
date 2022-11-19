8e72c09435422b9e9614ae584212be958b40e024
ToyinH <byhisgraceabba@gmail.com>
2022-11-19 02:49:08 +0000

    printing 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @m: first digit
+ * @n: second digit
+ * @l: third digit
+ * Return: 0 if successful
+ *
+ */
+int main (void)
+{
+	int m, n, l;
+
+	for ()
+}
