bb620a9c56f653951db50952b2cfa4fa3ea9351d
ToyinH <byhisgraceabba@gmail.com>
2022-11-28 21:24:56 +0000

    print all elements of `list_t` list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
 
 /**
  * print_list - a function that prints all the elements of a list_t list
+ * @h: header
  * Return: the number of nodes
  */
 
@@ -14,9 +15,6 @@ size_t print_list(const list_t *h)
 
 	c = 0;
 	current = h;
-
-	if (current == NULL)
-		printf("[0] (nil)\n");
 
 	while (current != NULL)
 	{
