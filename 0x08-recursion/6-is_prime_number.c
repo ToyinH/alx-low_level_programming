5d7ae5fc6f7a644915725aecffa669fef1e2e685
ToyinH <byhisgraceabba@gmail.com>
2023-06-07 22:47:17 +0000

    function to check if a number is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	int i;
+	if (n <= 1)
+	{
+		return 0;
+	}
+
+	if (i == 1)
+	{
+		return 1;
+	}
+	if (n % i == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		i = i - 1;
+		is_prime_number(n);
+	}
+}
