0ccba6b743541a07aaaf2004b36e0aae33d48521
ToyinH <byhisgraceabba@gmail.com>
2023-06-07 16:52:31 +0000

    function that returns x raised to power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -19,16 +19,15 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 
-	/* 
-	 * you may want to add this section of code too. 
-	 * either way, it will work. 
+	/**
+	 * you may want to add this section of code too.
+	 * either way, it will work.
 	 * else if (y == 1)
 	* {
 	*	return (x);
 	* }
 	*
 	*/
-	
 	else
 	{
 		return (x * _pow_recursion(x, y - 1));
