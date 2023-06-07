aff44b984f09bd3543dacaed664ac7add6e6ca85
ToyinH <byhisgraceabba@gmail.com>
2023-06-07 21:07:37 +0000

    function that returns the square root of numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,21 +4,27 @@
 
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
+ * test - testing the guess for the squareroot
+ * closeEnough - function that return true or false to close enough guess to
+ * square root of number
+ * betterGuess - function that returns the betterGuess than the closeEnough
  * @n - the number
- * return: the square of the number 
+ * @x - the number to find its squareroot
+ * @g - the guess number
+ * return: the square of the number
  *
  *
  */
 
-double test (double x, double g);
-bool closeEnough (double a, double b);
-double betterGuess (double x, double g);
+double test(double x, double g);
+bool closeEnough(double a, double b);
+double betterGuess(double x, double g);
 
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
-		return (1);
+		return (-1);
 	}
 	else if (n == 0 || n == 1)
 	{
@@ -26,24 +32,24 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (int)test((double)n, 1.0);
+		return ((int)test((double)n, 1.0));
 	}
 
 }
 
-double test (double x, double g)
+double test(double x, double g)
 {
-	if (closeEnough(x/g, g))
+	if (closeEnough(x / g, g))
 	{
 		return (g);
 	}
 	else
 	{
-		return test (x, betterGuess(x, g));
+		return test(x, betterGuess(x, g));
 	}
 }
 
-bool closeEnough (double a, double b)
+bool closeEnough(double a, double b)
 {
 	if (fabs(a - b) < 0.001)
 	{
@@ -57,7 +63,7 @@ bool closeEnough (double a, double b)
 
 double betterGuess(double x, double g)
 {
-	return ((g + x/g) / 2);
+	return ((g + x / g) / 2);
 }
 
 
