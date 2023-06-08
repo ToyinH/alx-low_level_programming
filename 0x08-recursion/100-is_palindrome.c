1d2a69962629975ae7a2fd4682093a49513135c0
ToyinH <byhisgraceabba@gmail.com>
2023-06-08 10:48:27 +0000

    a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- *
- *
- *
+ * is_palindrome - function that checks if a word is a pallindrome
+ * @s: string
+ * @low: starting from the beginning of the string
+ * @high: starting from the end of the string
+ * Return: return 1 if a pallindrome and 0 if not
  */
 
 int is_palindrome_helper(char *s, int low, int high);
@@ -11,27 +13,42 @@ int strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	int len;
+
 	len = strlen_recursion(s);
 
-	return is_palindrome_helper(s, 0, len - 1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
 
+/**
+ * is_palindrome_helper - function that helps the palindrome function above
+ * @s: string
+ * @low: starting from the beginning of the string
+ * @high: starting from the end of the string
+ *
+ * Return: 1 if pallindrome and 0 if not
+ */
 int is_palindrome_helper(char *s, int low, int high)
 {
 	if (low >= high)
 	{
-		return 1;
+		return (1);
 	}
 	if (s[low] != s[high])
 	{
-		return 0;
+		return (0);
 	}
-	return is_palindrome_helper(s, low + 1, high - 1);
+	return (is_palindrome_helper(s, low + 1, high - 1));
 }
+
+/**
+ * strlen_recursion - function that returns the len of a string
+ * @str: string
+ *
+ * Return: return string length
+ */
 
 int strlen_recursion(char *str)
 {
-	
 	if (*str != '\0')
 	{
 		return (1 + strlen_recursion(str + 1));
