dcea7d762666a7bbaa41f33e346034f31c7f0abd
ToyinH <byhisgraceabba@gmail.com>
2023-06-14 00:34:34 +0000

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,12 +3,20 @@
 #include "main.h"
 
 /**
+ * argstostr - function that concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
  *
+ * Return: return pointer to string
  */
+int get_str_len(char *s);
+void copy_string(char *dest, char *src);
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, m, n, total_len;
-	int len = 0;
+	int i, j;
+	int total_len = 0;
+	int position = 0;
 	char *new_str;
 
 	if (ac == 0 || av == NULL)
@@ -18,28 +26,39 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			len++;
-		}
+		total_len += get_str_len(av[i]) + 1; /* +1 to care for new line*/
 	}
 
-	total_len = len + ac + 1;
-
-	new_str = malloc(total_len * sizeof(char));
-
-	if (new_str == NULL)
+	new_str = malloc((sizeof(char) * total_len) + 1);
+	
+	for (j = 0; j < ac; j++)
 	{
-		return (NULL);
+		copy_string((new_str + position), av[j]);
+		position += get_str_len(av[j]);
+		new_str[position++] = '\n';
 	}
-	for (m = 0, k = 0; m < ac; m++, k++)
-	{
-		for (n = 0; av[m][n] != '\0'; n++)
-		{
-			new_str[k] = av[m][n];
-		}
-		new_str[k] = '\n';
-	}
-	new_str[k] = '\0';
+	new_str[position] = '\0';
 	return (new_str);
+}
+
+int get_str_len(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+void copy_string(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 }
