b21fe3a9ebad7b58ad2956a35b0b2de787a441e8
ToyinH <byhisgraceabba@gmail.com>
2023-06-14 14:48:06 +0000

    Write a function that splits a string into words.

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *
+ */
+char **allocate_mem(char *s);
+
+char **strtow(char *str)
+{
+	int i;
+	int word_num = 0;
+	int position = 0;
+
+	char **grid1 = allocate_mem(str);
+	if (grid1 == NULL)
+	{
+		return (NULL);
+	}
+
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			grid1[word_num][i] = '\0';
+			word_num++;
+			position = 0;
+		}
+		else
+		{
+			grid1[word_num][position] = str[i];
+			position++;
+		}
+	}
+	grid1[word_num][position] = '\0';
+	grid1[word_num +1] = NULL;
+	return (grid1);
+}
+
+char **allocate_mem(char *s)
+{
+	int i;
+	int word_count = 0;
+	char **grid;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ')
+		{
+			word_count++;
+		}
+	}
+	grid = (char **)malloc(sizeof(char *) * (word_count + 1)); 
+	/* +1 for the NULL element*/
+	{
+		if (grid == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (grid);
+}
