c0c7d34fac2138e46586c6eee98364684d235b21
ToyinH <byhisgraceabba@gmail.com>
2023-06-16 04:25:15 +0000

    a program that multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - multiply two integers
+ * @numA: first number
+ * @numB: second number
+ *
+ * Return: return 0 if successful
+ */
+int mul(int numA, int numB);
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!isdigit(num1) || !isdigit(num2))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	result = mul(num1, num2);
+	printf("%d\n", result);
+	return (0);
+
+}
+/*
+ * mul- function to multiply two integers
+ * @numA: first num
+ * @numB: second num
+ *
+ * Return: return product
+ */
+
+int mul(int numA, int numB)
+{
+	int product;
+	product = numA * numB;
+	return (product);
+}
