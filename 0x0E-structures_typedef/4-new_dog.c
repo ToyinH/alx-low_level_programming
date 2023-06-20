870791b48dfe3eb95de7af30c4034fd19a1c600d
ToyinH <byhisgraceabba@gmail.com>
2023-06-20 13:59:54 +0000

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,10 +4,7 @@
 
 /**
  * new_dog - funtion to create new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- *
+ *@string: string
  * Return: return pointer to new dog created
  */
 
@@ -19,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int owner_lent;
 	dog_t *create_dog;
 
-	create_dog = (dog_t *) malloc(sizeof(dog_t));
+	create_dog = malloc(sizeof(dog_t));
 	if (create_dog == NULL)
 	{
 		return (NULL);
@@ -27,8 +24,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/*create memory to store name, free mem and return NULL if it failed*/
 	name_lent = len(name);
-	name = (char *)malloc(sizeof(char) * (name_lent + 1));
-	if (name == NULL)
+	create_dog->name = malloc(sizeof(char) * (name_lent + 1));
+	if (create_dog->name == NULL)
 	{
 		free(create_dog);
 		return (NULL);
@@ -36,22 +33,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/*create memory to store owner, free mem and return NULL if it failed*/
 	owner_lent = len(owner);
-	owner = (char *)malloc(sizeof(char) * (owner_lent + 1));
-	if (owner == NULL)
+	create_dog->owner = malloc(sizeof(char) * (owner_lent + 1));
+	if (create_dog->owner == NULL)
 	{
+		free(create_dog->name);
 		free(create_dog);
-		free(name);
 		return (NULL);
 	}
 
 	/*copy entered string into new dog created*/
-	
 	create_dog->name = string_copy(create_dog->name, name);
 	create_dog->age = age;
 	create_dog->owner = string_copy(create_dog->owner, owner);
 
 	return (create_dog);
 }
+
+/**
+ * len - function to cal the length of string
+ * @string: the string
+ *
+ * Return: return string length
+ */
 
 int len(char *string)
 {
@@ -64,15 +67,24 @@ int len(char *string)
 	return (i);
 }
 
+/**
+ * string_copy - funtion to copy string
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: return pointer to copied string
+ */
+
 char *string_copy(char *s1, char *s2)
 {
-	int i = 0;
+	char *ptr = s1;
 
-	while (s1[i] != '\0')
+	while (*s2 != '\0')
 	{
-		s1[i] = s2[i];
+		*s1 = *s2;
+		s1++;
+		s2++;
 	}
-	s1[i] = '\0';
-	return (s1);
-
+	*s1 = '\0';
+	return (ptr);
 }
