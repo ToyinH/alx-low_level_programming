6e4ed249e0e71fd5c4311c350dbb849cb09429ce
ToyinH <byhisgraceabba@gmail.com>
2023-06-21 19:05:00 +0000

    a program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
