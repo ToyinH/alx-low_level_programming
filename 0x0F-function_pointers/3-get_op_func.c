92bb3e35341ce7c8b00e24aa7afc0bcab8fbff47
ToyinH <byhisgraceabba@gmail.com>
2023-06-21 19:43:42 +0000

    a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (ops[i].op == NULL)
-		{
-			return (NULL);
-		}
+		return (ops[i].f);
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
