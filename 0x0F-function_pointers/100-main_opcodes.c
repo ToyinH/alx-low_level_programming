1d602ce5dcd89743378913e5b95718659352c1e0
ToyinH <byhisgraceabba@gmail.com>
2023-06-22 04:41:25 +0000

    program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @num_bytes: num of bytes
+ *
+ * Return: 0 if succesful.
+ */
+int print_opcodes(int num_bytes);
+int main(int argc, char *argv[])
+{
+	int bytes = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes(bytes);
+	return (0);
+}
+
+/**
+ * print_opcodes - function to print opcodes of own function
+ * @num_bytes: number of bytes
+ *
+ * Return: 0 if successful
+ */
+
+int print_opcodes(int num_bytes)
+{
+	int i;
+	unsigned char *ptr = (unsigned char *)print_opcodes;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", ptr[i]);
+	}
+	printf("\n");
+	return (0);
+}
