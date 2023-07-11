912322c4aa58693275ffb3c9eb61607ded53bb97
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-11 04:57:06 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #define BUF_SIZE 1024
 /**
+ * main - program that copies the content of a file to another file
+ * @argc: arg count
+ * @argv: arg vector
  *
+ * Return: return 1 if successful
  */
+
 int main(int argc, char **argv)
 {
 	int fd_to, fd_from;
@@ -12,30 +17,15 @@ int main(int argc, char **argv)
 	const char *file_to = argv[2];
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (file_from == NULL)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	count = read(fd_from, buf, BUF_SIZE);
 	if (count == -1)
 	{
@@ -55,6 +45,10 @@ int main(int argc, char **argv)
 	}
 	close(fd_from);
 	close(fd_to);
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (1);
 }
 
