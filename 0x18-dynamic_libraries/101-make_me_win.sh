1c5fc5933878147e478fc71b0af99ad826c89e05
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-08-22 04:32:31 +0000

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o lib_print.so -fPIC custom_print.c
-export LD_PRELOAD=/$PWD/lib_print.so
+gcc -shared -o ../lib_print.so -fPIC custom_print.c
+export LD_PRELOAD=../lib_print.so
