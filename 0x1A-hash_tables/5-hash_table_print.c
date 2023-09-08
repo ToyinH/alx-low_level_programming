7f72838499ad38f6a3e2d12fe6a6209d0412d82d
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-08 12:42:58 +0000

    a function that prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - fuunction that prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int i, word = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	i = 0;
+	while (i < ht->size)
+	{
+		current = ht->array[i];
+		if (word < 1)
+		{
+			while (current != NULL)
+				{
+					printf("{");
+					printf("'%s': '%s'", current->key, current->value);
+					current = current->next;
+					word++;
+					break;
+				}
+		}
+		while (current != NULL)
+		{
+			printf(", '%s': '%s'", current->key, current->value);
+			word++;
+			current = current->next;
+		}
+		i++;
+	}
+	printf("}");
+	printf("\n");
+}
